7d87c6f7205902d923ba41dc450fd2ee18a90f8b
BrigYeet <orest.mokryy@gmail.com>
2022-06-09 13:08:24 +0300

    Lab 5

---
+++ db_lab5/src/main/java/ua/lviv/iot/controller/PersonController.java
@@ -0,0 +1,30 @@
+package ua.lviv.iot.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import ua.lviv.iot.model.entity.Person;
+import ua.lviv.iot.service.PersonService;
+
+public class PersonController extends GeneralController<Person, Integer, PersonService> {
+
+    public PersonController() {
+        super(PersonService.class);
+    }
+
+    public List<Person> findByInitials(String name, String surname) throws SQLException {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        Session session = configuration.buildSessionFactory().openSession();
+        PersonService service = new PersonService();
+        List<Person> people = null;
+        try {
+            people = service.findByInitials(name, surname, session);
+        } finally {
+            session.close();
+        }
+        return people;
+    }
+
+}
